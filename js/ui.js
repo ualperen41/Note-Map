eec9edabd7cc84e9ed5d35d00616b67e63602b2f
ualperen41 <alperenb.200021@gmail.com>
2025-08-21 18:55:39 +0300

    düzenleme

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,11 @@
-export const ui = {
+const uiElement = {
     aside:document.querySelector("aside"),
     asideTitle:document.querySelector("#title"),
     form:document.querySelector("aside form"),
     arrow:document.querySelector("#arrow"),
 };
 
-
+ export default uiElement;
 export const personIcon = L.icon({
     iconUrl:"../images/person.png",
     iconSize: [50,50],
